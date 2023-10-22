8967e49c396fdef7bd527393dae7fbc3174bcb64
Xavier Coll <xavier.coll@hotmail.com>
2023-10-22 16:51:42 +0200

    vite2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/snakepit/',
+  base: '/Front-Snakepit/',
   plugins: [react()],
 })
